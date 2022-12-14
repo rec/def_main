6b5a3db0a969ea6f0b71fb3ef085b11519168e7f
Tom Ritchford <tom@swirly.com>
2022-12-14 19:07:21 +0100

    Teach def_main about Poetry

--- def_main.py
+++ def_main.py
@@ -1,8 +1,14 @@
 from typing import Callable
+import inspect
 import sys
+
+MAINS = []
 
 
 def def_main(f: Callable) -> Callable:
+    s = inspect.stack()[1]
+    MAINS.append((f, s.filename, s.lineno))
+
     if f.__module__ == '__main__':
         f(*sys.argv[1:])
 
